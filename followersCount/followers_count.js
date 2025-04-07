f753955bc86ba1a54cd79ec676894bc57a2c689e
Ibukun <ibeksconde@gmail.com>
2025-04-07 15:54:56 +0100

    smaple iteration

---
+++ followersCount/followers_count.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+// function increaseCount() {
+//     count++;
+// }
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function checkCountValue() {
+    if (count===10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
+
+function reset() {
+    count = 0;
+    displayCount();
+    alert("Your Followers count has been reset!");
+}
